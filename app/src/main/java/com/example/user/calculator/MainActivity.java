a272f7ff8550293a44193c214d55f68fe585528d
wjm0101 <wjm01015@gmail.com>
2019-01-20 14:38:30 +0900

    asdf

--- app/src/main/java/com/example/user/calculator/MainActivity.java
+++ app/src/main/java/com/example/user/calculator/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.user.calculator;
 
+import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    static String Expression = new String(), fstExpression, secExpression = null;
+    static String Expression = new String(), fstExpression;
     static protected int index;
     static protected TextView mainTextView;
     static boolean isPointUsed = false;
@@ -16,9 +19,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     protected double Calculate(String a, String b, char x) {
+        if (a.length() == 0 || b.length() == 0 || a == null || b == null)
+            return 0;
+        Toast.makeText(getApplicationContext(), a, Toast.LENGTH_LONG);
+        Toast.makeText(getApplicationContext(), b, Toast.LENGTH_LONG);
         double res = 0;
-        if (x == '＋') {
-            res = (Double.parseDouble(a) + Double.parseDouble(b));
+        switch(x) {
+            case '＋':
+                res = (Double.parseDouble(a) + Double.parseDouble(b));
+                break;
+            case '－':
+                res = (Double.parseDouble(a) - Double.parseDouble(b));
+                break;
+            case '×':
+                res = (Double.parseDouble(a) * Double.parseDouble(b));
+                break;
+            case '÷':
+                if (Double.parseDouble(b) == 0)
+                    return 0;
+                res = (Double.parseDouble(a) / Double.parseDouble(b));
+
         }
         return res;
     }
@@ -26,10 +46,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        fstExpression = null;
+        isPointUsed = false;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mainTextView = (TextView)findViewById(R.id.mainTV);
-        Button btnList[] = new Button[12], btnBackspace = (Button)findViewById(R.id.btnBackspace), btnPlus = (Button)findViewById(R.id.btnPlus), btnEqual = (Button)findViewById(R.id.btnequal);
+        Button btnList[] = new Button[12], btnBackspace = (Button)findViewById(R.id.btnBackspace);
+        Button btnSymbol[] = new Button[4], btnEqual = (Button)findViewById(R.id.btnequal);
         btnList[0] = (Button)findViewById(R.id.btn0);
         btnList[1] = (Button)findViewById(R.id.btn1);
         btnList[2] = (Button)findViewById(R.id.btn2);
@@ -41,6 +64,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnList[8] = (Button)findViewById(R.id.btn8);
         btnList[9] = (Button)findViewById(R.id.btn9);
         btnList[10] = (Button)findViewById(R.id.btnpoint);
+        btnSymbol[0] = (Button)findViewById(R.id.btnPlus);
+        btnSymbol[1] = (Button)findViewById(R.id.btnSubtract);
+        btnSymbol[2] = (Button)findViewById(R.id.btnMultiply);
+        btnSymbol[3] = (Button)findViewById(R.id.btnDivision);
         btnBackspace.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 if (Expression.length() != 0) {
@@ -50,33 +77,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     Expression = Expression.substring(0, Expression.length() - 1);
                     mainTextView.setText(Expression);
                 }
+
+                else {
+                    Expression = Expression.substring(0,0);
+                    mainTextView.setText(Expression);
+                }
             }
         });
         btnBackspace.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                Expression = Expression.substring(0, 0);
+                Expression = Expression.substring(0,0);;
+                isPointUsed = false;
                 mainTextView.setText(Expression);
                 return false;
             }
         });
-        btnPlus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (secExpression == null) {
-                    fstExpression = Expression;
-                }
+        for (int i = 0; i < 4; i++) {
+            btnSymbol[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Button b = (Button)v;
+                    String buttonText = b.getText().toString();
+                    if (fstExpression == null)
+                        fstExpression = Expression;
+                    else {
+                        Double a = new Double(Calculate(fstExpression, Expression, calSymbol));
+                        fstExpression = a.toString();
+                    }
 
-                else {
-                    Double a = new Double(Calculate(fstExpression, secExpression, calSymbol));
-                    fstExpression = a.toString();
+                    Expression = Expression.substring(0,0);;
+                    isPointUsed = false;
+                    mainTextView.setText(Expression);
+                    calSymbol = buttonText.charAt(0);
+                    Log.d("asdfasdfsadf", fstExpression);
                 }
-
-                Expression = Expression.substring(0, 0);
-                mainTextView.setText(Expression);
-                calSymbol = '＋';
-            }
-        });
+            });
+        }
         for (int i = 0; i < 10; i++) {
             btnList[i].setOnClickListener(this);
         }
@@ -96,12 +133,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             @Override
             public void onClick(View v) {
-                if (secExpression == null)
+                if (fstExpression == null)
                     return;
-                else {
-                    
-                }
 
+                else {
+                    Double res = Calculate(fstExpression, Expression, calSymbol);
+                    Expression = res.toString();
+                    mainTextView.setText(Expression);
+                }
+                fstExpression = null;
             }
         });
     }
